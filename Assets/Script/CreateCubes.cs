118b5e93426a3e7ecebaf7dbfb35c8c508dd2d8b
ArenHansy <aren.hansy@gmail.com>
2019-05-21 19:44:23 +0900

    apply gitignore

--- Assets/Script/CreateCubes.cs
+++ Assets/Script/CreateCubes.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -16,21 +17,51 @@ public class CreateCubes : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Joint joint;
+        RunLoop((d, h, w) =>
+        {
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.transform.position = new Vector3(w - WIDTH_2, h - HEIGHT_2, d - DEPTH_2);
+            Rigidbody cubeRigidbody = cube.AddComponent<Rigidbody>();
+            cubeRigidbody.useGravity = false;
+            Cubes[d, h, w] = cube;
+        });
 
+        RunLoop((d, h, w) =>
+        {
+            GameObject cube = cubes[d, h, w];
+            FixedJoint fixedJoint = cube.AddComponent<FixedJoint>();
+            if (w <= 0)
+            {
+                GameObject cube1 = cubes[d, h, w - 1];
+            }
+            else if (w >= WIDTH - 1)
+            {
+
+            }
+            else
+            {
+
+            }
+        });
+
+        joint = Cubes[0, 0, 1].AddComponent<Joint>();
+        joint.connectedBody = Cubes[0, 0, 2].GetComponent<Rigidbody>();
+
+    }
+
+    public void RunLoop(Action<int, int, int> action)
+    {
         for (int d = 0; d < DEPTH; d++)
         {
             for (int h = 0; h < HEIGHT; h++)
             {
                 for (int w = 0; w < WIDTH; w++)
                 {
-
-                    GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                    cube.transform.position = new Vector3(w - WIDTH_2, h - HEIGHT_2, d - DEPTH_2);
-                    Cubes[d, h, w] = cube;
+                    action(d, h, w);
                 }
             }
         }
-
     }
 
 
