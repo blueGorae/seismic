92bbbbe56035f5c4a20c279a40f0cc49a59b8d33
Seongtae <seongtae0205@postech.ac.kr>
2019-06-06 16:23:35 +0900

    add S 3D

--- Assets/Script/S_Wave_3D.cs
+++ Assets/Script/S_Wave_3D.cs
@@ -7,7 +7,9 @@ public class S_Wave_3D : MonoBehaviour
 {
     private Vector3[] originalVertices;
     private Mesh mesh;
-
+    private Vector3 startPoint = new Vector3(0.0f, 0.0f, 0.0f);
+    private float v = 6.0f;
+    private float A = 1.0f;
     void Start()
     {
         mesh = GetComponent<MeshFilter>().mesh;
@@ -36,10 +38,11 @@ public class S_Wave_3D : MonoBehaviour
         // sine wave with an amplitude of 1 unit and a period of 2π units, 
         // traveling with a speed of 1 unit per second.
         // Change this to your own wave function.
+        float d = Mathf.Sqrt(Mathf.Pow(origin.x, 2) + Mathf.Pow(origin.y, 2) + Mathf.Pow(origin.z, 2));
         return new Vector3(
-             origin.x,
-             origin.y,
-             origin.z + Mathf.Sin(origin.x + timeCode)
+             origin.x, 
+             origin.y + (float)A*(Mathf.Cos(2 * Mathf.PI * (Time.fixedTime + (d/v)))),
+             origin.z + (float)A*(Mathf.Cos(2 * Mathf.PI * (Time.fixedTime - (d/v))))
         );
     }
 }
