9b8539d21da8070f5087a6b734c829c45913dbe2
ArenHansy <aren.hansy@gmail.com>
2019-06-06 21:38:17 +0900

    slider value changed

---
+++ Assets/Script/Manger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Manger : MonoBehaviour
+{
+    public Slider densitySlider;
+    public Text densityValue;
+    public Slider magnitudeSlider;
+    public Text magnitudeValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        densityValue.text = densitySlider.value.ToString();
+        magnitudeValue.text = magnitudeSlider.value.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void UpdateDensitySlider(float value)
+    {
+        densityValue.text = value.ToString();
+    }
+
+    public void UpdateMagnitudeSlider(float value)
+    {
+        magnitudeValue.text = value.ToString();
+    }
+}
