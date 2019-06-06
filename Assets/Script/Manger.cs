ed665ef77aa0f7b30ea5d677f0c98c95f72b4a0f
ArenHansy <aren.hansy@gmail.com>
2019-06-06 22:21:51 +0900

    add ui callback

--- Assets/Script/Manger.cs
+++ Assets/Script/Manger.cs
@@ -8,20 +8,25 @@ public enum Wave { NONE, W_P, W_S, W_EARTHQUAKE };
 
 public class Manger : MonoBehaviour
 {
-
-
     public ToggleGroup targetGroup;
     public Toggle target2D;
     public Toggle target3D;
     public Toggle targetGrassland;
     public Slider densitySlider;
     public Text densityValue;
+    private float density2D;
+    private float density3D;
+    private float densityGrassland;
     public Slider magnitudeSlider;
     public Text magnitudeValue;
+    private float magnitude2D;
+    private float magnitude3D;
+    private float magnitudeGrassland;
     public ToggleGroup waveGroup;
     public Toggle waveP;
     public Toggle waveS;
     public Toggle waveEarthquake;
+
 
     // Start is called before the first frame update
     void Start()
@@ -40,12 +45,93 @@ public class Manger : MonoBehaviour
 
     public void UpdateDensitySlider(float value)
     {
+        switch (CurrentTarget())
+        {
+            case Target.NONE:
+                break;
+            case Target.T_2D:
+                density2D = value;
+                break;
+            case Target.T_3D:
+                density3D = value;
+                break;
+            case Target.T_GRASSLAND:
+                densityGrassland = value;
+                break;
+        }
         densityValue.text = value.ToString();
     }
 
     public void UpdateMagnitudeSlider(float value)
     {
+        switch (CurrentTarget())
+        {
+            case Target.T_2D:
+                magnitude2D = value;
+                break;
+            case Target.T_3D:
+                magnitude3D = value;
+                break;
+            case Target.T_GRASSLAND:
+                magnitudeGrassland = value;
+                break;
+        }
         magnitudeValue.text = value.ToString();
+    }
+
+    public void UpdateTargetGroup()
+    {
+        float density;
+        float magnitude;
+        switch (CurrentTarget())
+        {
+            case Target.T_2D:
+                density = density2D;
+                magnitude = magnitude2D;
+                break;
+            case Target.T_3D:
+                density = density3D;
+                magnitude = magnitude3D;
+                break;
+            case Target.T_GRASSLAND:
+                density = densityGrassland;
+                magnitude = magnitudeGrassland;
+                break;
+            default:
+                return;
+        }
+        densitySlider.value = density;
+        magnitudeSlider.value = magnitude;
+    }
+
+    public float Density(Target target)
+    {
+        switch (target)
+        {
+            case Target.T_2D:
+                return density2D;
+            case Target.T_3D:
+                return density3D;
+            case Target.T_GRASSLAND:
+                return densityGrassland;
+        }
+        return 0f;
+    }
+
+    public float Magnitude(Target target)
+    {
+        switch (target)
+        {
+            case Target.NONE:
+                break;
+            case Target.T_2D:
+                return magnitude2D;
+            case Target.T_3D:
+                return magnitude3D;
+            case Target.T_GRASSLAND:
+                return magnitudeGrassland;
+        }
+        return 0f;
     }
 
     public Target CurrentTarget()
