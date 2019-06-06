733da22ce5e942e6b17e5a8ebddb6605f01a0d1b
ArenHansy <aren.hansy@gmail.com>
2019-06-07 02:42:24 +0900

    add camera

--- Assets/Script/Manger.cs
+++ Assets/Script/Manger.cs
@@ -28,6 +28,7 @@ public class DensityVelocity
 
 public class Manger : MonoBehaviour
 {
+    public Camera camera;
     public ToggleGroup targetGroup;
     public Toggle target2D;
     public Toggle target3D;
@@ -122,9 +123,13 @@ public class Manger : MonoBehaviour
         {
             case Target.T_2D:
                 magnitude = magnitude2D;
+                camera.transform.position = new Vector3(0, 30, -30);
+                camera.transform.rotation = Quaternion.Euler(45, 0, 0);
                 break;
             case Target.T_3D:
                 magnitude = magnitude3D;
+                camera.transform.position = new Vector3(0, 30, -30);
+                camera.transform.rotation = Quaternion.Euler(45, 0, 0);
                 break;
             case Target.T_GRASSLAND:
                 magnitude = magnitudeGrassland;
